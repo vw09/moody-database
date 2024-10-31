b8e9b8d7392e4d1b56f9b6a207d074e5108b9b12
Sarah <151523107+vw09@users.noreply.github.com>
2024-10-31 17:00:43 +0100

    aanpassing in email lijn

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ import e from 'express';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, },
-    email: { type: String, required: true, },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     moodhistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mood', }],
     playlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist', }],
     likedSongs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song', }],
