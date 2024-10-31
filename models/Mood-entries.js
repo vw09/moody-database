c0fa2d5721ee8f93c29837c0ead61c8cae32b7f6
Sarah <151523107+vw09@users.noreply.github.com>
2024-10-31 16:23:22 +0100

    aanmaak mood-entries

---
+++ models/Mood-entries.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const moodEntrySchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    Date: { type: Date, default: Date.now },
+    mood: { type: String, required: true },
+    description: { type: String, required: true },
+    recommendedPlaylist: { type: String, required: true },
+    recommendedsong: { type: String, required: true },
+});
