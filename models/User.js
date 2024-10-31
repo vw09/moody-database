046f131baaf88fbe6fef6e68471a7c1a5137be2b
Sarah <151523107+vw09@users.noreply.github.com>
2024-10-31 14:44:17 +0100

    user model created

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import e from 'express';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, },
+    email: { type: String, required: true, },
+    moodhistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mood', }],
+    playlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist', }],
+    likedSongs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song', }],
+    createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
