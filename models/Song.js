6350f2a4cce13dc49a7283c96f75ca6b81b12027
Sarah <151523107+vw09@users.noreply.github.com>
2024-10-31 16:24:49 +0100

    aanmaak song

---
+++ models/Song.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    artist: { type: String, required: true },
+    album: { type: String, required: true },
+    genre: { type: String, required: true },
+    mood: { type: String, required: true },
+});
