aa3f7763c3ddda5ba9d5ebe6a77dd3bec792268b
Sarah <151523107+vw09@users.noreply.github.com>
2024-11-01 14:55:51 +0100

    bug fixes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import User from './models/User.js'; // Zorg ervoor dat deze import gebruikt wordt in je code
+import User from './models/User.js'; 
 import routeIndex from './routes/index.js';
 
-// Laad de omgevingsvariabelen vanuit het .env-bestand
+
 dotenv.config();
 
-// Log de MONGODB_URI naar de console
+
 console.log('MongoDB URI:', process.env.MONGODB_URI);
 
 const app = express(); 
@@ -20,7 +20,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Maak verbinding met MongoDB zonder verouderde opties
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Failed to connect to MongoDB", err));
