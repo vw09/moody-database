5dba37dd53c95dfc9a2bfa284ffe7fbc6b9b9b6a
Sarah <151523107+vw09@users.noreply.github.com>
2024-11-13 12:19:50 +0100

    username veranderd naar user met een id bij

--- models/Mood.js
+++ models/Mood.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const moodSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     date: { type: Date, default: Date.now }, 
     mood: { 
         type: String, 
