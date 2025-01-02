144029c0f3f78b8bf19e0dd2fce6f951d94f6f85
Sarah <151523107+vw09@users.noreply.github.com>
2025-01-03 00:02:56 +0100

    CallbackURL fix

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://moody-database.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
@@ -99,7 +99,7 @@ app.get('/auth/google/callback', passport.authenticate('google', { failureRedire
   (req, res) => {
     const user = req.user;
     const { redirectUri } = JSON.parse(req.query.state);
-    const fallbackUri = 'exp://localhost:10000'; // Update for your Expo app
+    const fallbackUri = 'exp://localhost:3000'; // Update for your Expo app
 
     const userInfo = {
       id: user._id,
