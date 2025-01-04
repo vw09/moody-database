911a655f2d1e58812d4e72b1c05dec4c3cdfb6f1
Sarah <151523107+vw09@users.noreply.github.com>
2025-01-04 15:56:40 +0100

    string change

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   username:  { type: String, required: true },
-  name:      { type: String, required: true },
   email:     { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
